d13547068325a9c30f37d426865212524d52004d
Eric Ross <eric.ross@hp.com>
2018-11-07 09:59:03 -0800

    Latest updates

--- sparksh.sh
+++ sparksh.sh
@@ -1,1 +1,1 @@
-spark-2.3.1-bin-hadoop2.7/bin/spark-shell -v --driver-class-path postgresql.jar
+spark-2.3.1-bin-hadoop2.7/bin/spark-shell -v --driver-class-path postgresql.jar --driver-memory 4G --executor-memory 32G --executor-cores 16
